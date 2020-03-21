35ddf1233031fba2f807b19688496510a94f6775
ben4code <nnaemeka.obioha@gmail.com>
2020-03-21 12:11:01 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <h1>Rick and Morty</h1>        
+      <p>Pick your favourite episode!</p>
+    </div>
+  );
+}
+
+export default App;
