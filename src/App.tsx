4bf8f27d691d2c3a50208afdbcd6de3be6bf2e0a
ben4code <nnaemeka.obioha@gmail.com>
2020-03-22 20:12:18 +0000

    Final commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter, Switch} from 'react-router-dom'
 import Home from './components/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Favs from './components/Favourites'
 import './app.css'
 
@@ -16,6 +17,7 @@ function App(): JSX.Element {
           <Route component={Home} exact path="/"/>
           <Route component={Favs} exact path="/favs"/>
         </Switch>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
