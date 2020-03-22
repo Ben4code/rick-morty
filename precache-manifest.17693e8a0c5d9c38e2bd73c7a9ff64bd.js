c1e9126a81c76e2666f6df5e90e431f26728ebcc
ben4code <nnaemeka.obioha@gmail.com>
2020-03-22 22:06:22 +0000

    Updates

---
+++ precache-manifest.17693e8a0c5d9c38e2bd73c7a9ff64bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3480a6194f73971b70ef904bad65ae3d",
+    "url": "/rick-morty/index.html"
+  },
+  {
+    "revision": "a78e5262f77d0619b89c",
+    "url": "/rick-morty/static/css/main.7ab7ea21.chunk.css"
+  },
+  {
+    "revision": "e360700b59894430a143",
+    "url": "/rick-morty/static/js/0.e17fb99a.chunk.js"
+  },
+  {
+    "revision": "d0027798955ef6c4ed93",
+    "url": "/rick-morty/static/js/3.be1d11d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-morty/static/js/3.be1d11d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a78e5262f77d0619b89c",
+    "url": "/rick-morty/static/js/main.ea195746.chunk.js"
+  },
+  {
+    "revision": "47afeca47d06f8c49755",
+    "url": "/rick-morty/static/js/runtime-main.f2273b13.js"
+  }
+]);
